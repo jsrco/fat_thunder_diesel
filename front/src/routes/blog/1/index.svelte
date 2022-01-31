0dbf538d09d5432121265bd92b9a2d19c9fd4335
Ryan Orlando <jsrco@hotmail.com>
2022-01-30 19:19:32 -0800

    Create index.svelte

---
+++ front/src/routes/blog/1/index.svelte
@@ -0,0 +1,260 @@
+<script>
+    import Container from "$lib/components/containers/container.svelte";
+</script>
+
+<Container>
+    <div class="relative pt-16">
+        <div
+            class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full"
+        >
+            <div
+                class="relative h-full text-lg max-w-prose mx-auto"
+                aria-hidden="true"
+            >
+                <svg
+                    class="absolute top-12 left-full transform translate-x-32"
+                    width="404"
+                    height="384"
+                    fill="none"
+                    viewBox="0 0 404 384"
+                >
+                    <defs>
+                        <pattern
+                            id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                            x="0"
+                            y="0"
+                            width="20"
+                            height="20"
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect
+                                x="0"
+                                y="0"
+                                width="4"
+                                height="4"
+                                class="text-gray-700"
+                                fill="currentColor"
+                            />
+                        </pattern>
+                    </defs>
+                    <rect
+                        width="404"
+                        height="384"
+                        fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+                    />
+                </svg>
+                <svg
+                    class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+                    width="404"
+                    height="384"
+                    fill="none"
+                    viewBox="0 0 404 384"
+                >
+                    <defs>
+                        <pattern
+                            id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                            x="0"
+                            y="0"
+                            width="20"
+                            height="20"
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect
+                                x="0"
+                                y="0"
+                                width="4"
+                                height="4"
+                                class="text-gray-700"
+                                fill="currentColor"
+                            />
+                        </pattern>
+                    </defs>
+                    <rect
+                        width="404"
+                        height="384"
+                        fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+                    />
+                </svg>
+                <svg
+                    class="absolute bottom-12 left-full transform translate-x-32"
+                    width="404"
+                    height="384"
+                    fill="none"
+                    viewBox="0 0 404 384"
+                >
+                    <defs>
+                        <pattern
+                            id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                            x="0"
+                            y="0"
+                            width="20"
+                            height="20"
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect
+                                x="0"
+                                y="0"
+                                width="4"
+                                height="4"
+                                class="text-gray-700"
+                                fill="currentColor"
+                            />
+                        </pattern>
+                    </defs>
+                    <rect
+                        width="404"
+                        height="384"
+                        fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+                    />
+                </svg>
+            </div>
+        </div>
+        <div class="relative px-4 sm:px-6 lg:px-8">
+            <div class="text-lg max-w-prose mx-auto">
+                <h1>
+                    <span
+                        class="block text-base text-center text-gray-200 font-semibold tracking-wide uppercase"
+                        >01.</span
+                    >
+                    <span
+                        class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-red-500 sm:text-4xl"
+                        >Till I hook this up to Sanity</span
+                    >
+                </h1>
+                <p class="mt-8 text-xl text-gray-300 leading-8">
+                    This post started out as a letter to <a
+                        href="https://www.terryjaymes.com/"
+                        target="_blank"
+                    >
+                        <span
+                            class="font-bold text-indigo-400 hover:text-indigo-500"
+                            >Terry Jaymes Alive</span
+                        ></a
+                    >. Like that "<strong>inspirational ****er</strong>", its
+                    time to start the ball rolling on life goals.
+                </p>
+            </div>
+
+            <div
+                class="mt-8 text-lg text-gray-300 mx-auto px-0 md:px-4 lg:px-32"
+            >
+                <p>
+                    I have been meaning to create this site for a couple of
+                    weeks now. <br /><br />
+                    I recently had a baby girl. During the delivery there were complications.<br
+                    /><br />
+                    A rough epidural, a broken vacuum, and an overhead view of an
+                    episiotomy... shit hit the fan for baby's first moments.
+                    <br /><br />
+                    <figure>
+                        <img
+                            class="max-h-96 m-auto rounded-lg"
+                            src="./assets/n1.png"
+                            alt="Nadja"
+                        />
+                        <figcaption
+                            class="m-auto text-center text-gray-400 text-sm"
+                        >
+                            "Relax, I am fine." Nadja said.
+                        </figcaption>
+                    </figure>
+                    <br />
+                    We spent 10 days in the NICU at a different hospital.The first
+                    day there, she stopped breathing while I was holding her hand.
+                    That was a lot to deal with.<br />
+                    <br />
+                    Plus, you aren't alone in the NICU. You are there with other
+                    families. There was a baby born at 23 weeks in the ward. It's
+                    a massive amount of emotion and information to be around.
+                    <br /><br />
+                    To say the least, my focus and mojo have gone to shit. I can't
+                    meditate or shoot my bow. Trying to work happens.<br /><br
+                    />
+                    It's like I lost my connection with, everything.
+                    <br /><br />
+                    <figure>
+                        <img
+                            class="max-h-96 m-auto rounded-lg"
+                            src="./assets/IMG_6876.JPG"
+                            alt="PooPoo Point Hike"
+                        />
+                        <figcaption
+                            class="m-auto text-center text-gray-400 text-sm"
+                        >
+                            PooPoo Point Hike.
+                        </figcaption>
+                    </figure>
+                    <br />
+                    My back up is going for a run or walk in the woods. But the weather
+                    out here hasn't been stellar.<br /><br />
+                    It feels pretty close to getting popped in the nose and stunned
+                    when boxing. It takes a bit to realign your perception and understand
+                    that you got smacked in the face.
+                    <br /><br />
+                    In aftermath of a traumatic event, I am curious how people deal.
+                    How do you deal with these kinds of major events that leave you
+                    reeling?<br /><br />
+                    Even with excitement from those first 10 days, it has been the
+                    most positive experience of my life. <br /><br />
+                    And its inspiring.<br /><br />
+                    <figure>
+                        <img
+                            class="max-h-96 m-auto rounded-lg"
+                            src="./assets/tat.jpg"
+                            alt="40th Bday Tats"
+                        />
+                        <figcaption
+                            class="m-auto text-center text-gray-400 text-sm"
+                        >
+                            40th Bday Tats.
+                        </figcaption>
+                    </figure>
+                    <br />
+                    As I rush back to productivity, "fully inspired", I realize I
+                    had seen my wife's vagina snipped open by scissors too. Almost
+                    a process of rediscovery and hitting a brick wall all at once.
+                    <br /><br />
+                    For the reader, baby and mom are doing amazing. I swear my partner
+                    is part machine. For me this will just "take some time".<br
+                    /><br />
+                    <figure>
+                        <img
+                            class="max-h-96 m-auto rounded-lg"
+                            src="./assets/n2.png"
+                            alt="Nadja"
+                        />
+                        <figcaption
+                            class="m-auto text-center text-gray-400 text-sm"
+                        >
+                            Nadja says, "Hi hi hi!"
+                        </figcaption>
+                    </figure>
+                    <br />
+                    How do you deal with major life changing situations that leave
+                    you stunned? What do you do to start picking up the pieces of
+                    the aftermath? There isn't a spot for you to respond.
+                    <br /><br /> Those thoughts are for you if they mean
+                    anything.<br /><br />
+                    Going forward, I will begin to connect this site and grow it
+                    out. I have a
+                    <a href="https://www.sanity.io/" target="_blank">
+                        <span
+                            class="font-bold text-indigo-400 hover:text-indigo-500"
+                            >Sanity</span
+                        ></a
+                    >
+                    based back end ready to go. I need to get off my butt and
+                    write the serializer for that to happen.
+                    <br /><br />Expect progress about my game
+                    <a href="https://www.croplike.com/" target="_blank">
+                        <span
+                            class="font-bold text-indigo-400 hover:text-indigo-500"
+                            >Croplike</span
+                        ></a
+                    >, web dev moments of joy, or more inspirational memos as I
+                    garner fame and fortune.
+                </p>
+            </div>
+        </div>
+    </div>
+</Container>
